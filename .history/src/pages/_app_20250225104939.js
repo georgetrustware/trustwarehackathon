bb759e744824495ebf208e3815501e06da3a3b2d
george-PC <cars.drones@gmail.com>
2025-02-25 11:19:39 -0800

    telegram set up

---
+++ .history/src/pages/_app_20250225104939.js
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import { ThirdwebProvider } from 'thirdweb/react';
+
+
+export default function App({ Component, pageProps }) {
+  return
+  <ThirdwebProvider>
+    <Component {...pageProps} />;
+    </ThirdwebProvider>
+    
+}
---
+++ .history/src/pages/index_20250225110120.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>My Telegram Mini App</h1>
+      <p>Test page without wallet</p>
+    </div>
+  );
+}
