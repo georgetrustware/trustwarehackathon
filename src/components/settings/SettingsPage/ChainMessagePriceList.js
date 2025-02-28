2901a0b456deb03e9781bdddfc38091c14424515
Malachy Chiagozie Olua <malachy@trustware.io>
2025-02-28 20:43:51 +0100

    added files

---
+++ src/components/settings/SettingsPage/ChainMessagePriceList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChainMessagePriceList({chainPrices}) {
+  return (
+    <div>ChainMessagePriceList</div>
+  )
+}
+
+export default ChainMessagePriceList
---
+++ src/components/settings/SettingsPage/EditChainPrice.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function EditChainPrice() {
+  return (
+    <div>
+        Select for chainId
+        Select for token
+        Select for privacyLevel
+        Input for price
+        Cancel
+        Save
+    </div>
+  )
+}
+
+export default EditChainPrice
